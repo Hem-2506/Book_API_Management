9de7269f551c67f9fd1c35723f89bbba74429253
Hem-2506 <hemant.hs2506@gmail.com>
2021-07-17 00:33:54 +0700

    add microservices and validations

--- database/book.js
+++ database/book.js
@@ -2,8 +2,14 @@ const mongoose = require("mongoose");
 
 // Creating a book schema
 const BookSchema = mongoose.Schema({
-    ISBN: String, 
-    title: String,
+    ISBN: {type: String,
+    required: true,
+minLength: 8, maxLength:10,
+}, 
+    title: {type: String,
+        required: true,
+    minLength: 8, maxLength:10,
+    },
     pubDate: String,
     numOfPage: Number,
     language: String,
